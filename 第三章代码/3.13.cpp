2b012e5672ed6acc69c778ff52543cb042d3e2df
jiaoyuhua <382041135@qq.com>
2018-10-29 16:34:38 +0800

    10-29

--- 第三章代码/3.13.cpp
+++ 第三章代码/3.13.cpp
@@ -1,37 +1,60 @@
-#include<iostream.h>
-class B
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<iostream>
+
+#include<cstring>
+
+using namespace std;
+class Student
 {
-	int x, y;
+	char *specialty;
 public:
-	B()
-	{
-		x = y = 0;
-		cout << "conl\t";
-	}
-	B(int i)
-	{
-		x = i; y = 0;
-		cout << "con2\t";
-	}
-	B(int i, int j)
-	{
-		x = i; y = j;
-		cout << "con3\t";
-	}
-	~B()
-	{
-		cout << "Des\t";
-	}
+	Student(char *pSpec = 0);
+	~Student();
+	void show();
+	Student(const Student &r);
 };
+Student::Student(char *pSpec)
+{
+	if (pSpec)
+	{
+		specialty = new char[strlen(pSpec) + 1];
+		strcpy(specialty, pSpec);
+	}
+	else specialty = 0;
+}
+Student::~Student()
+{
+	if (specialty)
+		delete[]specialty;
+}
+void Student::show()
+{
+	cout << "specialty= " << specialty << endl;
+}
+Student::Student(const Student &r)
+{
+	if (r.specialty)
+	{
+		specialty = new char[strlen(r.specialty) + 1];
+		strcpy(specialty, r.specialty);
+	}
+	else specialty = 0;
+}
 int main()
 {
-	B *ptr;
-	ptr = new B[3];
-	ptr[0] = B();
-	ptr[1] = B(1);
-	ptr[2] = B(2,3);
-	delete []ptr;
+	Student zhang("computer");
+	Student wang(zhang);
+	zhang.show();
+	wang.show();
 
-	system("pause");
+	getchar();
 	return 0;
+
+
+
 }
+
+
+
